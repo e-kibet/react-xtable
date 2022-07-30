936233974ce9c8b84f75a29774ba4f7124bded86
Evans Kiptarus Kibet <evanskibet114@gmail.com>
2022-07-30 18:30:15 +0300

    initial project commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { headers, data } from './dummy/table';
+import './components/XTable.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <table className='table'>
+          <div>
+            <div>headering is</div>
+          </div>
+          <thead>
+            <tr style={{ backgroundColor: '#F3F2F1', fontSize: '17px', padding: '30px' }}>
+              {headers.map((d, i) => {
+                return <td key={i}>{d.name}</td>
+              })}
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((d, i) => {
+              return <tr key={i} className="tr__data">
+                {Object.values(d).map((val, j) => {
+                  return <td key={j}>{val}</td>
+                })}
+              </tr>
+            })}
+          </tbody>
+        </table>
       </header>
     </div>
   );
