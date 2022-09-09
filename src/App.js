286ef4951954d1c655eb47cb3baf2a83307583cc
Evans Kiptarus Kibet <evanskibet114@gmail.com>
2022-09-09 17:58:10 +0300

    fix by adding prettier

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import './App.css';
-import { headers, data } from './dummy/table';
-import './components/XTable.css'
+import "./App.css";
+import { headers, data } from "./dummy/table";
+import "./components/XTable.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <table className='table'>
+        <table className="table">
           <thead>
-            <tr style={{ border: '2px solid red', fontSize: '17px', padding: '30px' }}>
+            <tr
+              style={{
+                border: "2px solid red",
+                fontSize: "17px",
+                padding: "30px",
+              }}
+            >
               {headers.map((d, i) => {
-                return <td key={i}>{d.name}</td>
+                return <td key={i}>{d.name}</td>;
               })}
             </tr>
           </thead>
           <tbody>
             {data.map((d, i) => {
-              return <tr key={i} className="tr__data">
-                {Object.values(d).map((val, j) => {
-                  return <td key={j}>{val}</td>
-                })}
-              </tr>
+              return (
+                <tr key={i} className="tr__data">
+                  {Object.values(d).map((val, j) => {
+                    return <td key={j}>{val}</td>;
+                  })}
+                </tr>
+              );
             })}
           </tbody>
         </table>
--- src/dummy/table.js
+++ src/dummy/table.js
@@ -1,43 +1,55 @@
-export const headers = [{
-    name: "ID"
-}, {
-    name: "Full Name"
-}, {
-    name: "Age"
-}, {
-    name: "Profession"
-}, {
-    name: "Country"
-}, {
-    name : "Date Created"
-}]
+export const headers = [
+  {
+    name: "ID",
+  },
+  {
+    name: "Full Name",
+  },
+  {
+    name: "Age",
+  },
+  {
+    name: "Profession",
+  },
+  {
+    name: "Country",
+  },
+  {
+    name: "Date Created",
+  },
+];
 
-export const data = [{
+export const data = [
+  {
     id: "#10",
     name: "Amos Doe",
     age: "20",
     profession: "Software engineer",
     country: "Kenya",
-    date: "2022-10-30 10:00:00"
-},{
+    date: "2022-10-30 10:00:00",
+  },
+  {
     id: "#10",
     name: "Amos Doe",
     age: "20",
     profession: "Software engineer",
     country: "Kenya",
-    date: "2022-10-30 10:00:00"
-},{
+    date: "2022-10-30 10:00:00",
+  },
+  {
     id: "#10",
     name: "Amos Doe",
     age: "20",
     profession: "Software engineer",
     country: "Kenya",
-    date: "2022-10-30 10:00:00"
-},{
+    date: "2022-10-30 10:00:00",
+  },
+  {
     id: "#10",
     name: "Amos Doe",
     age: "20",
     profession: "Software engineer",
     country: "Kenya",
-    date: "2022-10-30 10:00:00"
-}]
+    date: "2022-10-30 10:00:00",
+  },
+];
